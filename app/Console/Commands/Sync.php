39092a07166269291db8c3fa0ee2aac4798fead1
Justin Romutap <cjustinr12@gmail.com>
2020-08-09 00:01:25 +0800

    create manufacturer sycing

--- app/Console/Commands/Sync.php
+++ app/Console/Commands/Sync.php
@@ -61,6 +61,45 @@ class Sync extends Command
                 }
                 echo 'syncing DONE';
                 break;
+
+            case 'manufacturer':
+                $token_details = storeToken();
+
+                foreach ($json_data as $key => $value) {
+                    $query = 'SELECT  items.sku, resellers_profiles.reseller_name FROM `items` 
+                        JOIN `resellers_profiles` ON items.username_id = resellers_profiles.username_id
+                        WHERE sku = '."'".$value->sku."'".'';
+                    $consolebetsy_item = DB::select($query);
+                    if ($consolebetsy_item) {
+                        $console_manufacturer = $consolebetsy_item[0]->reseller_name;
+                        if ($console_manufacturer != $value->manufacturer) {
+                            $ch = curl_init($token_details['domain']."/rest/all/V1/products/".$value->sku);
+                            $json = new \stdClass;
+                            $json->product = [
+                                "custom_attributes" => [
+                                    [
+                                        "attribute_code" => "manufacturer",
+                                        "value" => $console_manufacturer
+                                    ],
+                                ]
+                            ];
+
+                            $json  = json_encode($json);
+                            
+                            $curlOptions = array(
+                                CURLOPT_CUSTOMREQUEST => "PUT",
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_POSTFIELDS => $json,
+                                CURLOPT_HTTPHEADER => array( "Content-type: application/json", "Authorization: Bearer ".json_decode($token_details['token']))
+                                );
+                                
+                                curl_setopt_array( $ch, $curlOptions );
+                                
+                                $response = curl_exec( $ch );
+                        }
+                    }
+                }
+                break;
             
             default:
                 # code...
